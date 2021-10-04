4d4b3721ef4e4a11ce9f911fcddb70a87feef4a4
Sajad Bagherzadeh <s.bagherzadeh@outlook.com>
2021-10-04 13:37:10 +0330

    Add project files.

---
+++ InterfaceSegregationPrinciple/IAccountRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{
+    public interface IAccountRepository :ILogin, IRegister, IForgotPassword
+    {
+
+    }
+
+    public interface ILogin
+    {
+        bool Login(string username, string password);
+        void Logout(string username);
+    }
+
+    public interface IRegister
+    {
+        int Register(string username, string password, string email);
+    }
+
+    public interface IForgotPassword
+    {
+        void ForgotPassword(string username);
+    }
+    public class AccountRepository : IAccountRepository
+    {
+        public int Register(string username, string password, string email)
+        {
+            return 1;
+        }
+
+        public void ForgotPassword(string username)
+        {
+            Console.WriteLine("Email Sent.");
+        }
+
+        public bool Login(string username, string password)
+        {
+            return true;
+        }
+
+        public void Logout(string username)
+        {
+            Console.WriteLine("Logout.");
+        }
+    }
+
+
+    public class LoginController : ILogin
+    {
+        public bool Login(string username, string password)
+        {
+            Console.WriteLine("Login");
+            return true;
+        }
+
+        public void Logout(string username)
+        {
+            Console.WriteLine("Logout");
+        }
+    }
+}
---
+++ InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{
+    class Program
+    {
+        public class Document
+        {
+            public string Content { get; set; }
+        }
+
+        public interface IMachine:IPrinter,IFax,IScan
+        {
+            
+        }
+        public interface IPrinter
+        {
+            void Print(Document d);
+        }
+        public interface IFax
+        {
+            void Fax(Document d);
+        }
+        public interface IScan
+        {
+            void Scan(Document d);
+        }
+
+        public class OldPrinter:IPrinter
+        {
+            public void Print(Document d)
+            {
+                Console.WriteLine("Old Printer Can Print.");
+            }
+        }
+        
+        public class MultiFunctionPrinter : IMachine
+        {
+            public void Print(Document d)
+            {
+                Console.WriteLine("Can Print ...");
+            }
+
+            public void Fax(Document d)
+            {
+                Console.WriteLine("Can Fax ...");
+            }
+
+            public void Scan(Document d)
+            {
+                Console.WriteLine("Can Scan ...");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IAccountRepository accountRepository = new AccountRepository();
+            //accountRepository.Login()
+            ILogin login = new AccountRepository();
+            //login.Login()
+            ILogin login2 = new LoginController();
+            //login2.Logout();
+
+        }
+    }
+}
